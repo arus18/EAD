cd469e5192284a5d37ef8acb417832214394a4b0
nadarajah.a <arus.nadarajah@hitachi-dps.com>
2023-10-06 18:11:02 +0530

    init

---
+++ WebApplication1/Models/Playlist.cs
@@ -0,0 +1,19 @@
+namespace WebApplication1.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+
+public class Playlist {
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string username { get; set; } = null!;
+
+    [BsonElement("items")]
+    [JsonPropertyName("items")]
+    public List<string> movieIds { get; set; } = null!;
+
+}
---
+++ WebApplication1/User.cs
@@ -0,0 +1,16 @@
+namespace WebApplication1
+
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
